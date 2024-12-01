ad91b8a86ad1eed2c81c235bcd2dbe5f7ef02536
Mainf <matvei12519@yandex.ru>
2024-12-01 20:35:46 +0300

    кофе1

--- main.py
+++ main.py
@@ -1,43 +1,35 @@
 import sys
-import math
-import random
+import sqlite3
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QPushButton
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QPainter, QBrush, QColor, QPen
-draw_circle = False
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox, QHeaderView
+from PyQt6.QtSql import QSqlDatabase, QSqlQueryModel
 
 
 class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(300, 300, 650, 550)
-        self.clicked = QPushButton("Нажать", self)
-        self.clicked.move(275, 450)
-        self.clicked.resize(150, 100)
-        self.clicked.clicked.connect(self.shar)
+        uic.loadUi('main.ui', self)
+        self.coffetable.setVisible(False)
+        self.clicked.clicked.connect(self.active)
 
-    def shar(self):
-        global draw_circle
-        draw_circle = True
-        self.update()
-
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        if draw_circle:
-            radius = random.randint(10, 150)
-            center_x = self.width() // 2
-            center_y = self.height() // 2
-            red = random.randint(0, 255)
-            green = random.randint(0, 255)
-            blue = random.randint(0, 255)
-            brush = QBrush(QColor(red, green, blue))
-            painter.setBrush(brush)
-            painter.drawEllipse(center_x - radius, center_y - radius, 2 * radius, 2 * radius)
+    def active(self):
+        self.coffetable.setVisible(True)
+        db = QSqlDatabase.addDatabase('QSQLITE')
+        db.setDatabaseName('coffee')
+        if not db.open():
+            return
+        model = QSqlQueryModel()
+        query = "SELECT * FROM coffee"
+        model.setQuery(query)
+        if model.lastError().isValid():
+            return
+        self.coffetable.setModel(model)
+        header = self.coffetable.horizontalHeader()
+        header.setSectionResizeMode(1, QHeaderView.ResizeMode.Stretch)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = Calculator()
-    window.show()
+    ex = Calculator()
+    ex.show()
     sys.exit(app.exec())
